5b1e06008737feb34f8ebb8eab277a9b018da962
Jeff K <zerothehero33@gmail.com>
2014-09-25 14:21:38 -0400

    Initial commit with QUAIL and a TestSuite template in /ChromeQuail

---
+++ ChromeQuail/TestSuite.js
@@ -0,0 +1,19 @@
+var TestSuite = function() {
+  self.tests = [];
+}
+
+
+TestSuite.prototype.addTest = function() {}
+
+TestSuite.prototype.remTest = function() {}
+
+TestSuite.prototype.currentTests = function() {}
+
+TestSuite.prototype.run = function() {
+  /*$('#content').quail({ jsonPath : 'src/resources',
+                    guideline : 'wcag2a',
+                    accessibilityTests : accessibilityTests,
+                    testFailed : function(event) { ... },
+                    complete : function(event) { ... },
+                  });*/
+}
