e102db8fa12e54f002922db10ea67121a90cf26d
Jeff K <zerothehero33@gmail.com>
2014-09-25 14:36:35 -0400

    Added function bodies to all TestSuite methods

--- ChromeQuail/TestSuite.js
+++ ChromeQuail/TestSuite.js
@@ -1,19 +1,26 @@
 var TestSuite = function() {
-  self.tests = [];
+  this.tests = [];
 }
 
 
-TestSuite.prototype.addTest = function() {}
+TestSuite.prototype.addTest = function(test) {
+  this.tests.push(test);
+}
 
-TestSuite.prototype.remTest = function() {}
+TestSuite.prototype.remTest = function(test) {
+  var isSame = function(element, index, array) { element != test; };
+  this.tests = this.tests.filter(isSame);
+}
 
-TestSuite.prototype.currentTests = function() {}
+TestSuite.prototype.currentTests = function() {
+  for (test in this.tests) { console.log(test) };
+}
 
 TestSuite.prototype.run = function() {
-  /*$('#content').quail({ jsonPath : 'src/resources',
+  $('#content').quail({ jsonPath : '../quail-2.1.0/src/resources',
                     guideline : 'wcag2a',
                     accessibilityTests : accessibilityTests,
-                    testFailed : function(event) { ... },
-                    complete : function(event) { ... },
-                  });*/
+                    testFailed : function(event) {  },
+                    complete : function(event) {  },
+                  });
 }
