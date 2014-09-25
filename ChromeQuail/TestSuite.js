c4b87c60e314aca89f538365d2d3fd2551544da0
Jeff K <zerothehero33@gmail.com>
2014-09-25 14:45:46 -0400

    Added function members for test success and fail handlers.

--- ChromeQuail/TestSuite.js
+++ ChromeQuail/TestSuite.js
@@ -1,19 +1,26 @@
-var TestSuite = function() {
-  self.tests = [];
+var TestSuite = function(fail_handler, success_handler) {
+  this.tests = [];
+  this.failure = fail_handler;
+  this.success = success_handler;
 }
 
+TestSuite.prototype.addTest = function(test) {
+  this.tests.push(test);
+}
 
-TestSuite.prototype.addTest = function() {}
+TestSuite.prototype.remTest = function(test) {
+  var isSame = function(element, index, array) { element != test; };
+  this.tests = this.tests.filter(isSame);
+}
 
-TestSuite.prototype.remTest = function() {}
-
-TestSuite.prototype.currentTests = function() {}
+TestSuite.prototype.currentTests = function() {
+  for (test in this.tests) { console.log(test) };
+}
 
 TestSuite.prototype.run = function() {
-  /*$('#content').quail({ jsonPath : 'src/resources',
-                    guideline : 'wcag2a',
-                    accessibilityTests : accessibilityTests,
-                    testFailed : function(event) { ... },
-                    complete : function(event) { ... },
-                  });*/
+  $('#content').quail({ jsonPath : '../quail-2.1.0/src/resources',
+                        guideline : this.tests,
+                        testFailed : this.failure,
+                        complete : this.success,
+                      });
 }
